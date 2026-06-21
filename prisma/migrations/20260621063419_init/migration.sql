8002ca458f63dfa66bdc0a49e50a66409ee5894a
rahulch17 <Rahulchaudhary986740@gmail.com>
2026-06-21 12:20:53 +0545

    updated migration file removing old migration file

--- prisma/migrations/20260621063419_init/migration.sql
+++ prisma/migrations/20260621063419_init/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE "company" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "symbol" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "company_pkey" PRIMARY KEY ("id")
 );
@@ -13,14 +12,13 @@ CREATE TABLE "daily_stock" (
     "id" SERIAL NOT NULL,
     "company_id" INTEGER NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
-    "open" DOUBLE PRECISION NOT NULL,
-    "high" DOUBLE PRECISION NOT NULL,
-    "low" DOUBLE PRECISION NOT NULL,
     "close" DOUBLE PRECISION NOT NULL,
-    "volume" INTEGER NOT NULL,
 
     CONSTRAINT "daily_stock_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "company_symbol_key" ON "company"("symbol");
+
+-- AddForeignKey
+ALTER TABLE "daily_stock" ADD CONSTRAINT "daily_stock_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
